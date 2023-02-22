fcddd73d02229da1c57f972a72a7c6eef14d5f8a
Juan Felipe Rada <j.f.r.a1997@gmail.com>
2023-02-22 19:14:38 -0300

    style: Add styles to the header component

--- src/components/header.js
+++ src/components/header.js
@@ -13,21 +13,41 @@
 
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import styles from "../styles/header.module.css";
 
 export default function Header()
 {
+    const router = useRouter()
+
     return (
         <header className={styles.header}>
             <div className={`container ${styles.bar}`}>
-
-                <Image src="/img/logo.png" width={300} height={300} alt='Logo' />
+                <Link href="/" legacyBehavior>
+                    <Image src="/img/logo.png" width={300} height={300} alt='Logo' />
+                </Link>
 
                 <nav className={styles.nav} >
-                    <Link href="/"> Home</Link>
-                    <Link href="/us"> About us</Link>
-                    <Link href="/blog">Blog</Link>
-                    <Link href="/store">Store</Link>
+                    <Link href="/" legacyBehavior>
+                        <a className={router.pathname === '/' ? styles.active : ''}>
+                            Home
+                        </a>
+                    </Link>
+                    <Link href="/us" legacyBehavior>
+                        <a className={router.pathname === '/us' ? styles.active : ''}>
+                            About us
+                        </a>
+                    </Link>
+                    <Link href="/blog" legacyBehavior>
+                        <a className={router.pathname === '/blog' ? styles.active : ''}>
+                            Blog
+                        </a>
+                    </Link>
+                    <Link href="/store" legacyBehavior>
+                        <a className={router.pathname === '/store' ? styles.active : ''}>
+                            Store
+                        </a>
+                    </Link>
                 </nav>
 
             </div>
